da7ec93e6758e4236461b25a59c214445d8b66e3
Lucas Nascimento <lucasnsaraujo0310@gmail.com>
2023-09-11 22:55:08 -0300

    implementing prettier, eslint & tw class sort

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,16 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, jest: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "prettier",
+
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', "prettier", "react", "react-hooks", "@typescript-eslint"],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { Button } from "./components/ui/button";
+import { Button } from './components/ui/button'
+import { Separator } from './components/ui/separator'
+import { Github } from 'lucide-react'
 
 export function App() {
   return (
     <>
-      <div>
-        <Button>Criar novo usuário</Button>
+      <div className='min-h-screen'>
+        <div className='flex items-center justify-between border-b px-6 py-3'>
+          <h1 className='text-xl font-bold italic'>upload.ai</h1>
+          <div className='flex items-center gap-3'>
+            <span className='text-sm text-muted-foreground'>Desenvolvido por @lucasnsaraujo</span>
+            <Separator orientation='vertical' className='h-6' />
+            <Button variant='outline'>
+              <Github className='mr-2 h-4 w-4' />
+              Github
+            </Button>
+          </div>
+        </div>
       </div>
     </>
-  );
+  )
 }
